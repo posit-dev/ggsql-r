b096ac45dbbedf6ffe5baba089aaa449b3b27ea6
Thomas Lin Pedersen <thomasp85@gmail.com>
2026-04-30 13:02:15 +0200

    custom release bullets

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,12 @@
 .onLoad <- function(lib, pkg) {
   run_on_load()
 }
+
+release_bullets <- function() {
+  c(
+    "Bump rust version to match package version",
+    "Re-vendor rust dependencies",
+    "Run `rextendr::document()` to refresh extendr wrappers and Rd files",
+    "Run `npm run check` to rebuild `inst/htmlwidgets/ggsql_vega.js` from `srcts/`"
+  )
+}
